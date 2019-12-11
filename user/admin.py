edf384062f86f6144160928661da723d7a370c07
unknown <lucasxvirtual@gmail.com>
2019-12-11 00:42:12 -0300

    defining user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,96 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django import forms
+from .models import *
 
 # Register your models here.
+
+admin.site.site_header = 'MUMM Admin'
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'nickname', 'name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'nickname', 'name', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {
+            'fields': ('email',
+                       'password',
+                       'nickname',
+                       'name',
+                       'thumbnail',
+                       'birth_date',
+                       'biography',
+                       'cell_phone',
+                       'forgot_password_expiration',
+                       'is_deleted',
+                       'feed_updated',
+                       'verified_email',
+                       'created_at')
+        }),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'name', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email')
+
+        if not nickname:
+            raise ValueError('Users must have an nickname')
+
+        if not name:
+            raise ValueError('Users must have a name')
+
+        user = self.model(
+            email=email,
+            nickname=nickname,
+            name=name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, nickname, name, password):
+        user = self.create_user(
+            email,
+            nickname,
+            name,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField()
+    nickname = models.CharField(max_length=50, unique=True)
+    is_admin = models.BooleanField(default=False)
+    verified_email = models.BooleanField(default=False)
+    forgot_password_expiration = models.DateTimeField(default=timezone.now)
+    thumbnail = models.ImageField(null=True)
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    biography = models.CharField(max_length=240, null=True)
+    cell_phone = models.CharField(max_length=25, null=True)
+    is_deleted = models.BooleanField(default=False)
+    feed_updated = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
